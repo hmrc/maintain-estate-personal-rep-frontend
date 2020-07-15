dbbae603dec124e435f3ed16cebe2b37cb8e543d
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-07-15 10:21:55 +0100

    Navigation placeholders.

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -16,28 +16,18 @@
 
 package navigation
 
-import javax.inject.{Inject, Singleton}
-
-import play.api.mvc.Call
-import controllers.routes
-import pages._
 import models._
+import pages._
+import play.api.mvc.Call
 
-@Singleton
-class Navigator @Inject()() {
+trait Navigator {
 
-  private val normalRoutes: Page => UserAnswers => Call = {
-    case _ => _ => routes.IndexController.onPageLoad()
+  def nextPage(page: Page, mode: Mode, userAnswers: UserAnswers): Call
+
+  def yesNoNav(ua: UserAnswers, fromPage: QuestionPage[Boolean], yesCall: => Call, noCall: => Call): Call = {
+    ua.get(fromPage)
+      .map(if (_) yesCall else noCall)
+      .getOrElse(controllers.routes.SessionExpiredController.onPageLoad())
   }
 
-  private val checkRouteMap: Page => UserAnswers => Call = {
-    case _ => _ => routes.CheckYourAnswersController.onPageLoad()
-  }
-
-  def nextPage(page: Page, mode: Mode, userAnswers: UserAnswers): Call = mode match {
-    case NormalMode =>
-      normalRoutes(page)(userAnswers)
-    case CheckMode =>
-      checkRouteMap(page)(userAnswers)
-  }
 }
