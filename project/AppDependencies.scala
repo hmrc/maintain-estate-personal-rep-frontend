3c6035fcbf82e4c634aa713d6a05027fbb4ce4f3
carrielies <carrielies@users.noreply.github.com>
2020-11-24 14:55:45 +0000

    TRUS-3568 Remove Whitelisting

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,6 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-ui"                        % "8.12.0-play-27",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.4.0-play-26",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "2.25.0",
-    "uk.gov.hmrc"       %% "play-whitelist-filter"          % "3.4.0-play-27",
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4"
   )
 
