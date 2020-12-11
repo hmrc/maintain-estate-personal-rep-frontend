2298f9d927098264984f4f1dc55621c24d219de0
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-12-11 09:14:28 +0000

    template-fix: Update govuk-template to 5.60.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.8-play27",
     "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
-    "uk.gov.hmrc"       %% "govuk-template"                 % "5.56.0-play-27",
+    "uk.gov.hmrc"       %% "govuk-template"                 % "5.60.0-play-27",
     "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-27",
     "uk.gov.hmrc"       %% "domain"                         % "5.10.0-play-27",
     "uk.gov.hmrc"       %% "play-ui"                        % "8.12.0-play-27",
