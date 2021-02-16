54c7c2f2049efb12a02e43f96fde08743d90f015
carrielies <carrielies@users.noreply.github.com>
2021-02-16 10:58:15 +0000

    TRUS-3574 Remove filters

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,11 +7,11 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.8-play27",
     "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
-    "uk.gov.hmrc"       %% "govuk-template"                 % "5.60.0-play-27",
+    "uk.gov.hmrc"       %% "govuk-template"                 % "5.61.0-play-27",
     "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-27",
     "uk.gov.hmrc"       %% "domain"                         % "5.10.0-play-27",
     "uk.gov.hmrc"       %% "play-ui"                        % "8.21.0-play-27",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.4.0-play-26",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.5.0-play-27",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "2.25.0",
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
     "uk.gov.hmrc"       %% "play-language"                  % "4.10.0-play-27"
