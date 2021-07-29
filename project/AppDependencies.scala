e81ba6bcc328180dc354f3bd51bbb9bb9fa9080e
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2021-07-29 13:59:03 +0100

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "domain"                         % "5.10.0-play-27",
     "uk.gov.hmrc"       %% "play-ui"                        % "9.0.0-play-27",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.6.0-play-27",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "5.3.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "5.8.0",
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
     "uk.gov.hmrc"       %% "play-language"                  % "5.1.0-play-27"
   )
