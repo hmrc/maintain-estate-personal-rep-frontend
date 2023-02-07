f2bec2d3896adc689e92667456c97d70f54c8f5a
Victor Osipov <osipovvictor1994@gmail.com>
2023-02-07 11:45:47 +0000

    DDCE-3339 - Update deps, + sbt, + plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import play.core.PlayVersion
 
 object AppDependencies {
 
-  private val mongoHmrcVersion = "0.73.0"
-  private val playBootstrapVersion = "7.8.0"
+  private val mongoHmrcVersion = "0.74.0"
+  private val playBootstrapVersion = "7.13.0"
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "1.1.0-play-28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "6.3.0-play-28",
     "uk.gov.hmrc"         %% "domain"                         % "8.1.0-play-28",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % playBootstrapVersion,
@@ -16,16 +16,18 @@ object AppDependencies {
   )
 
   private lazy val test = Seq(
-    "org.scalatest"          %% "scalatest"             % "3.2.14",
-    "org.scalatestplus"      %% "mockito-4-6"           % "3.2.14.0",
-    "org.scalatestplus"      %% "scalacheck-1-17"       % "3.2.14.0",
-    "org.scalatestplus.play" %% "scalatestplus-play"    % "5.1.0",
-    "com.vladsch.flexmark"   % "flexmark-all"           % "0.62.2",
-    "org.jsoup"              %  "jsoup"                 % "1.15.3",
-    "com.typesafe.play"      %% "play-test"             % PlayVersion.current,
-    "com.github.tomakehurst" % "wiremock-standalone"    % "2.27.2",
-    "io.github.wolfendale"   %% "scalacheck-gen-regexp" % "1.0.0"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"         % playBootstrapVersion,
+    "org.scalatest"          %% "scalatest"                      % "3.2.15",
+    "org.scalatestplus"      %% "mockito-4-6"                    % "3.2.15.0",
+    "org.scalatestplus"      %% "scalacheck-1-17"                % "3.2.15.0",
+    "org.scalatestplus.play" %% "scalatestplus-play"             % "5.1.0",
+    "com.vladsch.flexmark"   % "flexmark-all"                    % "0.62.2",
+    "org.jsoup"              %  "jsoup"                          % "1.15.3",
+    "com.typesafe.play"      %% "play-test"                      % PlayVersion.current,
+    "com.github.tomakehurst" % "wiremock-standalone"             % "2.27.2",
+    "io.github.wolfendale"   %% "scalacheck-gen-regexp"          % "1.1.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
+
 }
