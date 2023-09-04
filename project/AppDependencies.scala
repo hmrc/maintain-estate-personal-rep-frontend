da4591299acca40da2b3862f88e64b15a84d4918
Slawek <13876870+slavvco@users.noreply.github.com>
2023-09-04 15:34:35 +0100

    DDCE-4175 | CSP review. (#94)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 object AppDependencies {
 
   private val mongoHmrcVersion = "1.3.0"
-  private val playBootstrapVersion = "7.20.0"
+  private val playBootstrapVersion = "7.21.0"
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.16.0-play-28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"         %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % playBootstrapVersion,
