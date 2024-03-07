0a76b24513b61c30f82c2626f24c048ab4227279
Gary Tate <66278690+garytate123@users.noreply.github.com>
2024-03-07 15:20:04 +0000

    DDCE-5381 Dependency updates for Tudor crown change (#96)
    * DDCE-5381 Dependency updates for Tudor crown change
    
    * DDCE-5381 Dependency updates for Tudor crown change
    
    * DDCE-5381 Address pr-bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-28"     % "8.5.0",
     "uk.gov.hmrc"         %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % playBootstrapVersion,
@@ -16,12 +16,12 @@ object AppDependencies {
 
   private lazy val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"         % playBootstrapVersion,
-    "org.scalatest"          %% "scalatest"                      % "3.2.16",
-    "org.scalatestplus"      %% "mockito-4-11"                   % "3.2.16.0",
-    "org.scalatestplus"      %% "scalacheck-1-17"                % "3.2.16.0",
+    "org.scalatest"          %% "scalatest"                      % "3.2.18",
+    "org.scalatestplus"      %% "mockito-4-11"                   % "3.2.18.0",
+    "org.scalatestplus"      %% "scalacheck-1-17"                % "3.2.18.0",
     "com.vladsch.flexmark"   % "flexmark-all"                    % "0.64.8",
-    "org.jsoup"              %  "jsoup"                          % "1.16.1",
-    "com.github.tomakehurst" % "wiremock-standalone"             % "2.27.2",
+    "org.jsoup"              %  "jsoup"                          % "1.17.2",
+    "org.wiremock"           % "wiremock-standalone"             % "3.4.2",
     "io.github.wolfendale"   %% "scalacheck-gen-regexp"          % "1.1.0"
   ).map(_ % Test)
 
