40a2d77f21afae24ef698604b88604cbb4e17550
EdCope <87376121+EdCope@users.noreply.github.com>
2024-04-30 10:21:33 +0100

    DDCE-5221: Play 3.0 Migration

--- app/controllers/SessionTimeoutController.scala
+++ app/controllers/SessionTimeoutController.scala
@@ -20,7 +20,6 @@ import config.FrontendAppConfig
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import play.api.{Configuration, Environment}
-import uk.gov.hmrc.play.bootstrap.config.AuthRedirects
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
 import scala.concurrent.Future
@@ -29,7 +28,7 @@ import scala.concurrent.Future
 class SessionTimeoutController @Inject()(val appConfig: FrontendAppConfig,
                                          val config: Configuration,
                                          val env: Environment,
-                                         mcc: MessagesControllerComponents) extends FrontendController(mcc) with AuthRedirects {
+                                         mcc: MessagesControllerComponents) extends FrontendController(mcc) {
 
 
   val keepAlive: Action[AnyContent] = Action.async { implicit request =>
