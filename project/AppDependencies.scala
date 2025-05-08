7c21548b54abd689f17a7128bca7962a79eb22e0
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-05-08 09:07:04 +0100

    DDCE-4609 - pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoHmrcVersion = "2.1.0"
+  private val mongoHmrcVersion = "2.6.0"
   private val playBootstrapVersion = "8.6.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "9.11.0",
     "uk.gov.hmrc"         %% "domain-play-30"                         % "10.0.0",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.0.0",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % playBootstrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                     % mongoHmrcVersion
   )
@@ -17,7 +17,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"         % playBootstrapVersion,
     "org.scalatestplus"      %% "scalacheck-1-17"                % "3.2.18.0",
     "io.github.wolfendale"   %% "scalacheck-gen-regexp"          % "1.1.0",
-    "org.jsoup"              %  "jsoup"                          % "1.18.1"
+    "org.jsoup"              %  "jsoup"                          % "1.20.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
