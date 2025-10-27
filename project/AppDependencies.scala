1a51cabc818d19c529fd590fcf0b21524d490860
Mithra Balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-10-27 12:57:15 +0000

    DDCE-6820 - gov uk rebranding (#109)
    * DDCE-6820 - gov uk rebranding
    
    * DDCE-6820 - pr bot comments
    
    * DDCE-6820 - pr comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoHmrcVersion = "2.6.0"
-  private val playBootstrapVersion = "9.16.0"
+  private val mongoHmrcVersion = "2.10.0"
+  private val playBootstrapVersion = "10.2.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.5.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.18.0",
     "uk.gov.hmrc"         %% "domain-play-30"                         % "11.0.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % playBootstrapVersion,
