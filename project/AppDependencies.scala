69a591c30761e51e3a5ab26308e929aaac14ce16
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2025-11-03 11:54:40 +0000

    DDCE-7578: Update repository.yaml to Match Standardized Format (#110)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,10 +3,10 @@ import sbt.*
 object AppDependencies {
 
   private val mongoHmrcVersion = "2.10.0"
-  private val playBootstrapVersion = "10.2.0"
+  private val playBootstrapVersion = "10.3.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.18.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.19.0",
     "uk.gov.hmrc"         %% "domain-play-30"                         % "11.0.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % playBootstrapVersion,
