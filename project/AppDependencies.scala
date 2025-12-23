b747b99dd18d9d170dd6660140ac9dcf9e559d6a
Harish Korada <174133905+harish-korada1@users.noreply.github.com>
2025-12-23 15:04:02 +0000

    DDCE-7779: [Estates]: Migrate all Estates Frontends to use the new gov.uk Service Navigation Component (4 - 6) (#111)
    * DDCE-7779: [Estates]: Migrate all Estates Frontends to use the new gov.uk Service Navigation Component (4 - 6)
    
    * DDCE-7779: [Estates]: addressing the comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoHmrcVersion = "2.10.0"
-  private val playBootstrapVersion = "10.3.0"
+  private val mongoHmrcVersion = "2.11.0"
+  private val playBootstrapVersion = "10.5.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.19.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.25.0",
     "uk.gov.hmrc"         %% "domain-play-30"                         % "11.0.0",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % playBootstrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                     % mongoHmrcVersion
   )
@@ -16,8 +16,7 @@ object AppDependencies {
   private lazy val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"         % playBootstrapVersion,
     "org.scalatestplus"      %% "scalacheck-1-17"                % "3.2.18.0",
-    "io.github.wolfendale"   %% "scalacheck-gen-regexp"          % "1.1.0",
-    "org.jsoup"              %  "jsoup"                          % "1.20.1"
+    "io.github.wolfendale"   %% "scalacheck-gen-regexp"          % "1.1.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
