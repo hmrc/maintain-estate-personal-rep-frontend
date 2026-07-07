b3d83f207d70250f7c5420c782aefc3188cfcb6a
Harish Korada <174133905+harish-korada1@users.noreply.github.com>
2026-07-07 09:53:11 +0100

    DDCE-8574: [Estates]: Upgrade play-frontend-hmrc in Estates Frontends (Repo 5 - 8) (#116)
    * DDCE-8574: [Estates]: Upgrade play-frontend-hmrc in Estates Frontends (Repo 5 - 8)
    
    * DDCE-8574: [Estates]: Upgrade play-frontend-hmrc in Estates Frontends (Repo 5 - 8)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   private val playBootstrapVersion = "10.7.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.9.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % playBootstrapVersion,
@@ -15,7 +15,7 @@ object AppDependencies {
 
   private lazy val test = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30" % playBootstrapVersion,
-    "org.scalatestplus"    %% "scalacheck-1-19"        % "3.2.19.0",
+    "org.scalatestplus"    %% "scalacheck-1-19"        % "3.2.20.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"
   ).map(_ % Test)
 
