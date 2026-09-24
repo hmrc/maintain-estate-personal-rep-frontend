883a907a395185a7bec0f61073fdf7f0e25b72d7
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2026-09-24 12:55:19 +0100

    DDCE-9193-Addressed PR bot comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoHmrcVersion     = "2.13.0"
+  private val mongoHmrcVersion     = "2.14.0"
   private val playBootstrapVersion = "10.8.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.13.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.14.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % playBootstrapVersion,
